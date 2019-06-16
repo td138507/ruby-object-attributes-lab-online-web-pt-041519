bebae913b1e6146022b69ecfeab9ff4681b5b12a
Taylor Doyle <tcd56@cornell.edu>
2019-06-16 02:03:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+  def name=(name)
+    @name = "Fido"
+  end
+
+  def name
+    @name = "Fido"
+  end
+
+
+
+  def breed
+    @breed = "Beagle"
+  end
+
+  def breed=(breed)
+    @breed = "Beagle"
+  end
+
+end
